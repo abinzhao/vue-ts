77e214657f758f74c81ec547cf293e73b7dfbf47
赵建斌 <zjb01237247@alibaba-inc.com>
2022-07-04 17:46:10 +0800

    feature(ui):  项目框架搭建

---
+++ src/pages/vueUse.vue
@@ -0,0 +1,42 @@
+<template>
+       
+    <h1>测试 vueUse 的鼠标坐标</h1>
+       
+    <h3>Mouse: {{ x }} x {{ y }}</h3>
+     
+</template>
+
+ 
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useMouse } from '@vueuse/core';
+import request from '@/utils/axios';
+import API from '@/api';
+
+export default defineComponent({
+    name: 'VueUse',
+    setup() {
+        const { x, y } = useMouse();
+
+        return {
+            x,
+            y,
+        };
+    },
+});
+
+const requestRes = async () => {
+    let result = await request({
+        url: '/api/xxx',
+        method: 'get',
+    });
+    console.log(result);
+};
+
+const requestRes = async () => {
+    let result = await API.login('zhangsan', '123456');
+    console.log(result);
+};
+
+console.log(requestRes);
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,49 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import viteCompression from "vite-plugin-compression";
+import {
+    createStyleImportPlugin,
+    ElementPlusResolve,
+} from 'vite-plugin-style-import';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    viteCompression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: "gzip",
-      ext: ".gz",
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        chunkFileNames: "static/js/[name]-[hash].js",
-        entryFileNames: "static/js/[name]-[hash].js",
-        assetFileNames: "static/[ext]/[name]-[hash].[ext]",
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
+    plugins: [
+        vue(),
+        createStyleImportPlugin({
+            resolves: [ElementPlusResolve()],
+            libs: [
+                // 如果没有你需要的resolve，可以在lib内直接写，也可以给我们提供PR
+                {
+                    libraryName: 'element-plus',
+                    esModule: true,
+                    resolveStyle: (name) => {
+                        return `element-plus/lib/theme-chalk/${name}.css`;
+                    },
+                    ensureStyleFile: true, // 忽略文件是否存在, 导入不存在的CSS文件时防止错误。
+                },
+            ],
+        }),
+    ],
+    resolve: {
+        //设置别名
+        alias: {
+            '@src': path.resolve(__dirname, 'src'),
         },
-      },
     },
-    chunkSizeWarningLimit: 1500,
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
+    server: {
+        port: 8080, //启动端口
+        hmr: {
+            host: '127.0.0.1',
+            port: 8080,
+        },
+        // 设置 https 代理
+        proxy: {
+            '/api': {
+                target: 'your https address',
+                changeOrigin: true,
+                rewrite: (path: string) => path.replace(/^\/api/, ''),
+            },
+        },
     },
-  },
 });
